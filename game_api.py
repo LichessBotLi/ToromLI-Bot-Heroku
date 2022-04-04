7e7d03b98fb55f3b67aa39379d736a180399d14f
LichessBotLi <102945249+LichessBotLi@users.noreply.github.com>
2022-04-04 20:59:01 +0530

    Update game_api.py

--- game_api.py
+++ game_api.py
@@ -22,9 +22,9 @@ class Game_api:
         game_queue_thread.start()
         
         
-        self.api.send_chat_message(self.game_id, "player", "Hello I am @Endogenetic-Bot made by @Soloboy4.")
+        self.api.send_chat_message(self.game_id, "player", "Hello I am @Endogenetic-Bot made by @Soloboy4.Best of Luck to you.")
         self.api.send_chat_message(self.game_id, "player", "Type !help or !cmd for a list of commands. I may respond too")
-        self.api.send_chat_message(self.game_id, "player", "Best of Luck to you! Try to Join my official team https://lichess.org/team/Endogenetic-Bot to chat with me
+        self.api.send_chat_message(self.game_id, "player", "Best of Luck to you! Try to Join my official team Endogenetic-Bot to chat with me")
 
         self.api.send_chat_message(self.game_id, "spectator", "Welcome friends to my game!")
         self.api.send_chat_message(self.game_id, "spectator", "Thanks for watching my games!")
