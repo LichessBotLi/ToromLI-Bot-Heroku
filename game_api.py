40fb9d6511cbe06f242f4afd75f3dcbe1a02d458
LichessBotLi <102945249+LichessBotLi@users.noreply.github.com>
2022-04-04 20:53:41 +0530

    Update game_api.py

--- game_api.py
+++ game_api.py
@@ -8,7 +8,7 @@ from lichess_game import Lichess_Game
 
 
 class Game_api:
-    def __init__(self, config: dict, api: API, username: str, game_id: str) -> None:
+    def __init__(self, config: dict, api: API, username: str, game_id: str):
         self.config: dict = config
         self.api = api
         self.username = username
@@ -17,9 +17,19 @@ class Game_api:
         self.ping_counter = 0
         self.game_queue = Queue()
 
-    def run_game(self) -> None:
+    def run_game(self):
         game_queue_thread = Thread(target=self._watch_game_stream, daemon=True)
         game_queue_thread.start()
+        
+        
+        self.api.send_chat_message(self.game_id, "player", "Hello I am @Endogenetic-Bot made by @Soloboy4.")
+        self.api.send_chat_message(self.game_id, "player", "Type !help or !cmd for a list of commands. I may respond too")
+        self.api.send_chat_message(self.game_id, "player", "Best of Luck to you! Try to Join my official team https://lichess.org/team/Endogenetic-Bot to chat with me
+
+        self.api.send_chat_message(self.game_id, "spectator", "Welcome friends to my game!")
+        self.api.send_chat_message(self.game_id, "spectator", "Thanks for watching my games!")
+        self.api.send_chat_message(self.game_id, "spectator", "Also type !cmd for commands and do not forget to Challenge me when I am not playing any game")
+
 
         while True:
             event = self.game_queue.get()
